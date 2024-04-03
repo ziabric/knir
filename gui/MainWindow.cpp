3d9c6e561f8c198a74bde331f8b652430d783dc5
ziabric <kurbet78@list.ru>
2024-04-04 00:57:08 +0300

    minor

--- gui/MainWindow.cpp
+++ gui/MainWindow.cpp
@@ -12,6 +12,8 @@ size(width_, height_)
     zoominIcon.loadFromMemory(tmp.data(), tmp.size());
     tmp = base64_decode(zoomoutIconString);
     zoomoutIcon.loadFromMemory(tmp.data(), tmp.size());
+    tmp = base64_decode(exporString);
+    exportIcon.loadFromMemory(tmp.data(), tmp.size());
 }
 MainWindow::~MainWindow()
 {
@@ -46,9 +48,9 @@ int MainWindow::mainloop()
             size = {(float)window->getSize().x, (float)window->getSize().y};
             
             drawInterface();
+            drawExportFile();
             drawFilesystem();
             drawImage();
-
 
             ImGui::PopFont();
 
@@ -65,48 +67,73 @@ void MainWindow::drawInterface()
     ImGui::SetWindowPos({0,0});
     ImGui::SetWindowSize({ImGui::GetWindowSize().x, size.y + 20});
     settingsWindowSize = ImGui::GetWindowSize();
-
-    if ( ImGui::ImageButton(filesystemIcon, {70, 70}) ) 
+    if ( ImGui::ImageButton(filesystemIcon, {35, 35}) ) 
     {
         filesystemOpenFlag = true;
     }
-    ImGui::Separator();
-    for (auto item : fileImage)
+    if (currentImage >= 0 && currentImage < fileImage.size() && fileImage[currentImage].filename != "") 
     {
-        // if ( item.filename == ImGui::Getwin )
-    }
-    ImGui::Separator();
-    if ( ImGui::Button("Median") )
-    {
-        // std::cout<<"Start"<<std::endl;
-        // al.clearData();
-        // al.setOrigImageSize(fileImage->getSize().x, fileImage->getSize().y);
-        // for (size_t i = 0; i < fileImage->getSize().x; i += 1)
-        // {
-        //     for (size_t j = 0; j < fileImage->getSize().y; j += 1 )
-        //     {
-        //         al.setOrigImagePixel(i, j, {fileImage->getPixel(i, j).b, fileImage->getPixel(i, j).g, fileImage->getPixel(i, j).r});
-        //     }
-        // }
-        // std::cout<<"Start median filter"<<std::endl;
-        // al.medianFilter(medianFilterRadius);
-        // std::cout<<"End median filter"<<std::endl;
-        // // newFileImage = fileImage;
-        // for (size_t i = 0; i < newFileImage.getSize().x; i += 1)
-        // {
-        //     for (size_t j = 0; j < newFileImage.getSize().y; j += 1 )
-        //     {
-        //         newFileImage.setPixel(i, j, {(sf::Uint8)al.getModImagePixel(i, j).r, (sf::Uint8)al.getModImagePixel(i, j).g, (sf::Uint8)al.getModImagePixel(i, j).b});
-        //     }
-        // }
-        // std::cout<<"End"<<std::endl;
-        // newFileImageFlag = true;
-    }
-    if ( ImGui::Button("Haar") )
-    {
+        ImGui::SameLine();
+        if ( ImGui::ImageButton(exportIcon, {35, 35}) ) 
+        {
+            exportFileFlag = true;
+        }
+        ImGui::Separator();
+        ImGui::Text("%s", fileImage[currentImage].filename.c_str());
+        ImGui::Separator();
+        if (ImGui::ImageButton(zoomoutIcon))
+        {
+            fileImage[currentImage].scale -= 0.1;
+        }
+        ImGui::SameLine();
+        if (ImGui::ImageButton(zoominIcon))
+        {
+            fileImage[currentImage].scale += 0.1;
+        }
+        ImGui::SliderFloat("##ScaleFoat", &fileImage[currentImage].scale, minFileScale, maxFileScale);
+        ImGui::Separator();
+        ImGui::Text("Point color");
+        ImGui::InputInt2("PointPos", fileImage[currentImage].pixelPos);
+        if ( fileImage[currentImage].pixelPos[0] >= 0 && fileImage[currentImage].pixelPos[0] <= fileImage[currentImage].image.getSize().x 
+            && fileImage[currentImage].pixelPos[1] >= 0 && fileImage[currentImage].pixelPos[1] <= fileImage[currentImage].image.getSize().y )
+        {
+            ImGui::Text("%s", std::string("B -- " + std::to_string(fileImage[currentImage].image.getPixel(fileImage[currentImage].pixelPos[0], fileImage[currentImage].pixelPos[1]).b)).c_str());
+            ImGui::Text("%s", std::string("G -- " + std::to_string(fileImage[currentImage].image.getPixel(fileImage[currentImage].pixelPos[0], fileImage[currentImage].pixelPos[1]).g)).c_str());
+            ImGui::Text("%s", std::string("R -- " + std::to_string(fileImage[currentImage].image.getPixel(fileImage[currentImage].pixelPos[0], fileImage[currentImage].pixelPos[1]).r)).c_str());
+        }
+        ImGui::Separator();
+        if ( ImGui::Button("Median") )
+        {
+            // std::cout<<"Start"<<std::endl;
+            // al.clearData();
+            // al.setOrigImageSize(fileImage->getSize().x, fileImage->getSize().y);
+            // for (size_t i = 0; i < fileImage->getSize().x; i += 1)
+            // {
+            //     for (size_t j = 0; j < fileImage->getSize().y; j += 1 )
+            //     {
+            //         al.setOrigImagePixel(i, j, {fileImage->getPixel(i, j).b, fileImage->getPixel(i, j).g, fileImage->getPixel(i, j).r});
+            //     }
+            // }
+            // std::cout<<"Start median filter"<<std::endl;
+            // al.medianFilter(medianFilterRadius);
+            // std::cout<<"End median filter"<<std::endl;
+            // // newFileImage = fileImage;
+            // for (size_t i = 0; i < newFileImage.getSize().x; i += 1)
+            // {
+            //     for (size_t j = 0; j < newFileImage.getSize().y; j += 1 )
+            //     {
+            //         newFileImage.setPixel(i, j, {(sf::Uint8)al.getModImagePixel(i, j).r, (sf::Uint8)al.getModImagePixel(i, j).g, (sf::Uint8)al.getModImagePixel(i, j).b});
+            //     }
+            // }
+            // std::cout<<"End"<<std::endl;
+            // newFileImageFlag = true;
+        }
+        if ( ImGui::Button("Haar") )
+        {
 
+        }
     }
-    ImGui::Separator();
+    if (fileImage.size()>0) ImGui::Separator();
     for (int i = 0; i < fileImage.size(); i +=1)
     {
         if ( ImGui::Button(std::string( fileImage[i].filename ).c_str()) )
@@ -119,6 +146,10 @@ void MainWindow::drawInterface()
             fileImage.erase(std::next(fileImage.begin(), i));
         }
     }
+    ImGui::Text("");
+    ImGui::Text("");
+    ImGui::Text("");
+
     ImGui::End();
 }
 void MainWindow::drawImage()
@@ -129,9 +160,14 @@ void MainWindow::drawImage()
         {
             ImGui::Begin(std::string( fileImage[i].filename ).c_str(), &(fileImage[i].openFlag), ImGuiWindowFlags_HorizontalScrollbar + ImGuiWindowFlags_NoCollapse);
             ImGui::SetWindowPos({settingsWindowSize.x + 10, 10}, ImGuiCond_Once);
-            ImGui::SetWindowSize({fileImage[i].image.getSize().x + 25, fileImage[i].image.getSize().y + 40}, ImGuiCond_Once);
+            ImGui::SetWindowSize({(float)fileImage[i].image.getSize().x + 25, (float)fileImage[i].image.getSize().y + 40}, ImGuiCond_Once);
             fileWindowPos = ImGui::GetWindowPos();
             fileWindowSize = ImGui::GetWindowSize();
+
+            if ( ImGui::IsWindowFocused() )
+            {
+                currentImage = i;
+            }
 
             fileImage[i].texture.loadFromImage(fileImage[i].image);
             fileImage[i].sprite.setTexture(fileImage[i].texture);
@@ -144,7 +180,7 @@ void MainWindow::drawImage()
             if ( ImGui::GetWindowPos().y < 0 ) ImGui::SetWindowPos({ImGui::GetWindowPos().x, 0});
             if ( ImGui::GetWindowPos().y + ImGui::GetWindowHeight() > size.y ) ImGui::SetWindowPos({ImGui::GetWindowPos().x, size.y - ImGui::GetWindowSize().y});
 
-            imagePopupMenu();
+            // imagePopupMenu();
 
             ImGui::End();
         }
@@ -236,9 +272,9 @@ void MainWindow::drawFilesystem()
     }
 }
 void MainWindow::openImage(fs::path pathToImage)
-{    
+{
     imageStruct tmp;
-    tmp.image.loadFromFile(pathToImage);
+    std::cout<<tmp.image.loadFromFile(pathToImage)<<std::endl;
     tmp.filename = pathToImage.filename().string();
     tmp.scale = 1;
 
@@ -255,20 +291,87 @@ void MainWindow::imagePopupMenu()
 
     if (ImGui::BeginPopup("filePopup", ImGuiWindowFlags_Popup))
     {
-        // if(ImGui::Button("1x"))
-        // {
-        //     fileScale = 1;
-        // }
-        // ImGui::SameLine();
-        // ImGui::SliderFloat("Scale", &fileScale, minFileScale, maxFileScale);
-        // ImGui::Separator();
-        // if (ImGui::RadioButton("B", fileChannel == ChannelType::Blue)) fileChannel = ChannelType::Blue;
-        // ImGui::SameLine();
-        // if (ImGui::RadioButton("G", fileChannel == ChannelType::Green)) fileChannel = ChannelType::Green;
-        // ImGui::SameLine();
-        // if (ImGui::RadioButton("R", fileChannel == ChannelType::Red)) fileChannel = ChannelType::Red;
-        // ImGui::SameLine();
-        // if (ImGui::RadioButton("All", fileChannel == ChannelType::All)) fileChannel = ChannelType::All;
         ImGui::EndPopup();
+    }
+}
+void MainWindow::drawExportFile()
+{
+    if (!filesystemOpenFlag && exportFileFlag && ImGui::Begin("Export to", &exportFileFlag, ImGuiWindowFlags_NoCollapse))
+    {
+        ImGui::SetWindowFocus();
+        std::stringstream ss(currentPath.string());
+        std::string s;
+        pathTmp.clear();
+        while(getline(ss, s, '/'))
+        {
+            if (s != "") pathTmp.push_back(s);
+        }
+        
+        ImGui::BeginChild("PathToFileExport", {-1, 50}, true, ImGuiWindowFlags_HorizontalScrollbar);
+        if (ImGui::Button("/"))
+        {
+            currentPath = "/";
+        }
+        for (int i = 0; i < pathTmp.size(); i += 1)
+        {
+            ImGui::SameLine();
+            if(ImGui::Button(pathTmp.at(i).c_str()))
+            {
+                fs::path newPath;
+                for (int j = 0; j <= i; j += 1)
+                {
+                    newPath += "/";
+                    newPath += pathTmp[j];
+                }
+                newPath += "/";
+                currentPath = newPath;
+            }
+            ImGui::SameLine();
+            ImGui::Text("/");
+        }
+        ImGui::SetScrollHereX(1);
+        ImGui::EndChild();
+
+        if (ImGui::BeginChild("fs", {0,ImGui::GetWindowSize().y - 150}, true))
+        {
+            ImGui::GetStyle().ButtonTextAlign = {0,0.5};
+            for ( const auto& item : fs::directory_iterator(currentPath) )
+            {
+                if (item.path().filename().c_str()[0] == '.') continue;
+
+                if (is_directory(item))
+                {
+                    if ( ImGui::Button(std::string(item.path().filename().string()).c_str(), {ImGui::GetWindowSize().x, 25}) )
+                    {
+                        currentPath = item;
+                    }
+                }
+                else if ( item.is_regular_file() )
+                {
+                    if (fileExt.find(fs::path(item).extension().string()) != fileExt.end())
+                    {
+                        ImGui::PushStyleColor(ImGuiCol_Button, {0.55, 0.714, 0, 1});
+                        ImGui::PushStyleColor(ImGuiCol_ButtonActive, {0.7, 0.714, 0, 1});
+                        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, {0.56, 0.814, 0, 1});
+                        // if (ImGui::Button(item.path().filename().c_str(), {ImGui::GetWindowSize().x, 25})) openImage(item.path());
+                        ImGui::PopStyleColor();
+                        ImGui::PopStyleColor();
+                        ImGui::PopStyleColor();
+                    }
+                    else
+                    {
+                        if (ImGui::Button(item.path().filename().c_str(), {ImGui::GetWindowSize().x, 25})){}
+                    }
+                }
+            }
+            ImGui::EndChild();
+
+            ImGui::InputTextWithHint("##FileName", "File name", exportFileName, 1024);
+            if (ImGui::Button("Save") && currentImage >= 0 && currentImage < fileImage.size() && fileImage[currentImage].filename != "")
+            {
+                fileImage[currentImage].image.saveToFile(currentPath.string() + exportFileName);
+            }
+        }
+        ImGui::End();
     }
 }
